46cff2c33d85c945f67da3d7f7e14ab274f1ff8f
mgrinko <glukym@gmail.com>
2020-01-31 11:30:46 +0200

    HashRouter was used

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 const baseName = process.env.REACT_APP_BASENAME || 'react_phone-catalog';
 
 ReactDOM.render(
-  <BrowserRouter basename={baseName}>
+  <HashRouter basename={baseName}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
