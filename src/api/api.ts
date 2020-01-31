721928f191635aacc93f12cf0a8922a6104e0ab7
mgrinko <glukym@gmail.com>
2020-01-31 11:37:53 +0200

    API_URL was changed to Heroku

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://phone-catalo-api.herokuapp.com';
 
 export const get = async<T>(url: string): Promise<T> => {
   const response = await fetch(API_URL + url);
