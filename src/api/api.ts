c0141348597a8e269bf0518b3565ad5acc52cc35
mgrinko <glukym@gmail.com>
2020-01-31 11:48:19 +0200

    typo was fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://phone-catalo-api.herokuapp.com';
+const API_URL = 'https://phone-catalog-api.herokuapp.com';
 
 export const get = async<T>(url: string): Promise<T> => {
   const response = await fetch(API_URL + url);
