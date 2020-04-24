4ba86ca5bfd2acbb636a2d5021831cd1f9cd7017
dima <tynindima@gmail.com>
2020-04-24 16:35:05 +0300

    Updates

---
+++ precache-manifest.8529a3620441af784e4739ab8d13f47d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e30511c4a4dbd1a271fabd615d0d479",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4af4f184d4466c4452ab",
+    "url": "./static/css/main.167d11f5.chunk.css"
+  },
+  {
+    "revision": "2a70f42a58a1c24cea4c",
+    "url": "./static/js/2.4606b757.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4606b757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af4f184d4466c4452ab",
+    "url": "./static/js/main.e8e2990c.chunk.js"
+  },
+  {
+    "revision": "7a9bffff0994439126ac",
+    "url": "./static/js/runtime-main.d5470fb7.js"
+  },
+  {
+    "revision": "e2c79ea5186b302b61903f9f09beefe4",
+    "url": "./static/media/Apple.e2c79ea5.svg"
+  },
+  {
+    "revision": "a178de2ec9601074da6529185bc2090c",
+    "url": "./static/media/Banner1.a178de2e.png"
+  },
+  {
+    "revision": "b69a42d81035c0e2298d5c98f608fe58",
+    "url": "./static/media/Drocher.b69a42d8.svg"
+  },
+  {
+    "revision": "ac82837dd6c9a2ca4cf7b588e822d155",
+    "url": "./static/media/Lexus.ac82837d.png"
+  },
+  {
+    "revision": "207b89669f1b01925aad930841f98230",
+    "url": "./static/media/Mont-Bold.207b8966.woff"
+  },
+  {
+    "revision": "6b95600360669632f828848489fb4be7",
+    "url": "./static/media/Mont-Book.6b956003.woff"
+  },
+  {
+    "revision": "3e8472dca0dc755fb399793983609c07",
+    "url": "./static/media/Mont-Regular.3e8472dc.woff"
+  },
+  {
+    "revision": "76ffaeaa6f43970e87066155f29f9659",
+    "url": "./static/media/Mont-SemiBold.76ffaeaa.woff"
+  },
+  {
+    "revision": "22d5c0b1e7b248421986bee67ad2c389",
+    "url": "./static/media/Phones1.22d5c0b1.png"
+  },
+  {
+    "revision": "c20ec296143f3cb99911262cb747d4f9",
+    "url": "./static/media/Phones2.c20ec296.png"
+  },
+  {
+    "revision": "699f70e26f5cd93e21a3705bb291fa61",
+    "url": "./static/media/Phones3.699f70e2.png"
+  },
+  {
+    "revision": "032a50cf1345af9ac76324ff5b0e4e2d",
+    "url": "./static/media/bag.032a50cf.svg"
+  },
+  {
+    "revision": "ba064880a8e6bce13a241bd23737f156",
+    "url": "./static/media/heart.ba064880.svg"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "./static/media/left.5ca5c1f5.svg"
+  },
+  {
+    "revision": "d2db6778ec36244ce1c638b723ee7322",
+    "url": "./static/media/lexus2.d2db6778.png"
+  },
+  {
+    "revision": "67be4a4a4d709745ad41a3578d953895",
+    "url": "./static/media/right.67be4a4a.svg"
+  },
+  {
+    "revision": "3d4e9107799312faec9376ef785b3a84",
+    "url": "./static/media/search.3d4e9107.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4a2ef0b30c7009914e017dc16a3d7a52.js"
+  "./precache-manifest.8529a3620441af784e4739ab8d13f47d.js"
 );
 
 self.addEventListener('message', (event) => {
